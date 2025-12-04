a88ceff1d9e0082bc4fb00e4c39cf50f90bcc421
Mateo William <albertojohn20041103@gmail.com>
2025-12-03 19:48:29 -0500

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    logging.info("Flask app started")
-    app.run(host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     logging.info("Flask app started")
+#     app.run(host="0.0.0.0", port=8000)
