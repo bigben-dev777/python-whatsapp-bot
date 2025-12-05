ec850e7f871a4e605fc72b8c2a5d78c92cf54e51
Mateo William <albertojohn20041103@gmail.com>
2025-12-04 22:50:19 -0500

    Uncomment logging for Flask app startup

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # logging.info("Flask app started")
+    logging.info("Flask app started")
     app.run(host="0.0.0.0", port=8000)
